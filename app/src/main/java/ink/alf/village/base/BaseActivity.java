7041625e0acc3d697b8a0900370ddd71f9a53d00
xupingwei <mailxpw@gmail.com>
2018-12-17 18:44:51 +0800

    statusbar

--- app/src/main/java/ink/alf/village/base/BaseActivity.java
+++ app/src/main/java/ink/alf/village/base/BaseActivity.java
@@ -3,10 +3,11 @@ package ink.alf.village.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import com.jaeger.library.StatusBarUtil;
 
 import butterknife.ButterKnife;
-import ink.alf.village.utils.SystemBarUtils;
+import ink.alf.village.R;
 
 
 /**
@@ -19,27 +20,13 @@ public  abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         ButterKnife.bind(this);
-        setSystemUIChangeListener();
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.themeColor), 0);
         initView(savedInstanceState);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        //焦点改变的时候，当Home键退出，重新从新进入等情况的处理。
-        SystemBarUtils.setStickyStyle(getWindow());
-    }
-
-    /**
-     * 监听系统UI的显示，进行特殊处理
-     */
-    private void setSystemUIChangeListener() {
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(visibility -> {
-            //当系统UI显示的时候（例如输入法显示的时候），再次隐藏
-            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                SystemBarUtils.setStickyStyle(getWindow());
-            }
-        });
     }
 
     /**
