b2525aca69b6c1b197abd515cc42db2ebd32a9b1
xupingwei <mailxpw@gmail.com>
2019-01-23 21:15:00 +0800

    微调

--- app/src/main/java/ink/alf/village/ui/fragment/FragmentMe.java
+++ app/src/main/java/ink/alf/village/ui/fragment/FragmentMe.java
@@ -1,36 +1,57 @@
 package ink.alf.village.ui.fragment;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.angel.view.SWImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import org.apache.commons.lang3.StringUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import ink.alf.village.R;
-import ink.alf.village.ui.activity.LoginActivity;
+import ink.alf.village.base.BaseFragment;
+import ink.alf.village.bean.vo.UserInfo;
 import ink.alf.village.utils.BlurUtils;
 
 /**
  * @author 13793
  */
-public class FragmentMe extends Fragment {
+public class FragmentMe extends BaseFragment {
 
     @BindView(R.id.ll_top)
     LinearLayout llHdearLayout;
 
     @BindView(R.id.rl_nickname_layout)
     RelativeLayout rlNicknameLayout;
+    @BindView(R.id.im_logo)
+    SWImageView imLogo;
+    @BindView(R.id.tv_nickname)
+    TextView tvNickname;
+    @BindView(R.id.tv_gender)
+    TextView tvGender;
+    @BindView(R.id.tv_auth_wx)
+    TextView tvAuthWx;
+    @BindView(R.id.tv_auth_alipay)
+    TextView tvAuthAlipay;
+    @BindView(R.id.tv_auth_phone)
+    TextView tvAuthPhone;
+    @BindView(R.id.tv_upgrade)
+    TextView tvUpgrade;
 
     private Unbinder unbinder;
 
@@ -49,9 +70,34 @@ public class FragmentMe extends Fragment {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.logo_bg);
         llHdearLayout.setBackground(new BitmapDrawable(BlurUtils.doBlur(bitmap, 10, 10)));
 
-        rlNicknameLayout.setOnClickListener(v -> {
-            startActivity(new Intent(getActivity(), LoginActivity.class));
-        });
+        UserInfo info = getUserInfo();
+        Glide.with(this)
+                .asBitmap()
+                .load(info.getHeadUrl())
+                .apply(new RequestOptions()
+                        .placeholder(R.mipmap.ic_avatar_default)
+                        .error(R.mipmap.ic_avatar_default)
+                        .diskCacheStrategy(DiskCacheStrategy.RESOURCE))
+                .into(imLogo);
+        tvNickname.setText(info.getNickName());
+        tvGender.setText(info.getGender() == null ? "未知" : info.getGender());
+        if (StringUtils.isEmpty(info.getOpenId())) {
+            tvAuthWx.setText("未认证");
+            tvAuthWx.setTextColor(getActivity().getResources().getColor(R.color.colorGray));
+
+        } else {
+            tvAuthWx.setText("已认证");
+            tvAuthWx.setTextColor(getActivity().getResources().getColor(R.color.colorOrange));
+        }
+        if (StringUtils.isEmpty(info.getAlipayAcount())) {
+            tvAuthAlipay.setText("未认证");
+            tvAuthAlipay.setTextColor(getActivity().getResources().getColor(R.color.colorGray));
+
+        } else {
+            tvAuthAlipay.setText("已认证");
+            tvAuthAlipay.setTextColor(getActivity().getResources().getColor(R.color.colorOrange));
+        }
+        tvAuthPhone.setText(info.getPhone());
     }
 
     @Override
